190e1e85aa774d2e01f42bdc3804aa3eb1a117c4
Aaron <agrav12825@gmail.com>
2022-10-03 15:53:09 -0500

    add arrow buttons viewer

--- src/components/Sections/PortfolioSection/PortfolioViewer/PortfolioViewer.tsx
+++ src/components/Sections/PortfolioSection/PortfolioViewer/PortfolioViewer.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 
 import styles from './PortfolioViewer.module.css';
 import reactMealsImg from '../../../../images/ReactMeals.png';
+import blogSiteImg from '../../../../images/blog-app.png';
 import View from './View';
+
+const views = [
+    {
+        href: 'https://react-meals-web-app-2d01b.firebaseapp.com/',
+        imgSrc: reactMealsImg,
+        altTxt: 'React Meals Web App'
+    },
+    {
+        href: 'https://frozen-falls-37428.herokuapp.com/',
+        imgSrc: blogSiteImg,
+        altTxt: 'Blog Web App' 
+    }
+] 
 
 const PortfolioViewer = () => {
     return (
         <div className={styles.viewer}>
-            <View 
-                href='https://react-meals-web-app-2d01b.firebaseapp.com/' 
-                imgSrc={reactMealsImg}
-                altTxt='React Meals Web App'
-            />
+            <button className={styles['viewer-btn']}>&lt;</button>
+            <View viewData={views[1]}/>
+            <button className={styles['viewer-btn']}>&gt;</button>
         </div>
     )
 };
---
+++ src/models/PortfolioViewer.ts
@@ -0,0 +1,5 @@
+export interface PortfolioViewData {
+    href: string,
+    imgSrc: string,
+    altTxt: string
+};
